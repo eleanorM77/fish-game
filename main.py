97adf85f272c2b12ed2883dcd6060854ec1b9b2e
eleanormncll <110623741+eleanorM77@users.noreply.github.com>
2022-08-04 17:22:29 +0000

    started defining main and added main call

--- main.py
+++ main.py
@@ -9,4 +9,18 @@ pygame.init()
 screen_info = pygame.display.Info()
 size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
- 
+
+#setting up clock
+clock = pygame.time.Clock()
+
+#set up color
+color = (0,127,255)
+
+#definining main
+def main():
+  pass
+
+
+#calling main
+if __name__ == __main__:
+  main()

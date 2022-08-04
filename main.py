d2af9387d0b786899cab22fbbcce9e45868a82e0
eleanormncll <110623741+eleanorM77@users.noreply.github.com>
2022-08-04 19:56:32 +0000

    added collision detection to move_fish function

--- main.py
+++ main.py
@@ -29,7 +29,17 @@ fish_image = pygame.transform.rotate(fish_image, 180-rotation)
 
 #defining move_fish
 def move_fish():
+  global fish_image
+  screen_info = pygame.display.Info()
   fish_rect.move_ip(speed)
+  if fish_rect.left < 0 or fish_rect.right >       screen_info.current_w:
+    speed[0] *= -1
+    fish_image = pygame.transform.flip(fish_image, True, False)
+    fish_rect.move_ip(speed[0],0)
+  if fish_rect.top < 0 or fish_rect.bottom > screen_info.current_h:
+    speed[1] *= -1
+    fish_image = pygame.transform.flip(fish_image, False, True)
+    fish_rect.move_ip(0, speed[1])
 
 #definining main
 def main():

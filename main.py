af446bdea1daac9ed1db38ef388f645c01b4bbac
eleanormncll <110623741+eleanorM77@users.noreply.github.com>
2022-08-04 17:13:33 +0000

    window size set up

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+#import pygame
 import sys, pygame
 from pygame.locals import *
 
+#initiate pygame
 pygame.init()
 
+#set window size
+screen_info = pygame.display.info()
+size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
